5dcbb9515f484fe87104e27cbf43a98509ca6d35
Michael A. Coughlin <abereanone@pm.me>
2026-06-10 15:29:58 -0400

    added help for nav

---
+++ migrations/0012_aos_notes.sql
@@ -0,0 +1,3 @@
+UPDATE aos SET notes = 'Tuesday - Ruck | Thursday - Bootcamp' WHERE slug = 'the_redzone';
+UPDATE aos SET notes = 'Bootcamp' WHERE slug = 'the_factory';
+UPDATE aos SET address = 'Impact Stadium, Marysville High School' WHERE slug = 'the_factory';
